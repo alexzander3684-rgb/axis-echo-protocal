a9c288fa41365bb9c3d567b4546856495a135724
alexzander3684-rgb <alexzander3684@gmail.com>
2025-12-28 08:29:16 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
